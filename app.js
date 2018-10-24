e2413ee42e4c5f35e53486bd18e831cce0dbd507
2mmtmc@gmail.com <mathieudemilloterrazzani@iMac-de-Mathieu-2.local>
2018-10-24 18:36:00 +0200

    Ajout des fonction javascript dans le fichier js pour rendre la page web interractive avec l'utilisateur

---
+++ app.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById("modal");
+
+function openModal(){
+	modal.style.top = "0px";
+}
+
+function annulationModal(){
+	modal.style.top = "-450px";
+}
+
+function validationModal(){
+	modal.style.top = "-450px";
+}
